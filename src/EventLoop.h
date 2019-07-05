6b15ef3bb4ccec16d46c76d986857051b656eaa8
hahagiraffe <646268684@qq.com>
2019-07-05 21:07:47 +0800

    update EventLoop

--- src/EventLoop.h
+++ src/EventLoop.h
@@ -3,6 +3,8 @@
  * @Author: haha_giraffe
  * @Date: 2019-06-21 16:07:14
  */
+#ifndef EVENTLOOP
+#define EVENTLOOP
 #include <pthread.h>
 
 class EventLoop{
@@ -18,9 +20,12 @@ public:
     }
     EventLoop* getEventLoopOfCurrentThread();
     bool isInLoopThread() const { return threadId_ ==static_cast<pid_t>(pthread_self());}
+    
 private:
     void abortNotInLoopThread();
     bool loop_;
     const pid_t threadId_;
 
 };
+
+#endif //EVENTLOOP
